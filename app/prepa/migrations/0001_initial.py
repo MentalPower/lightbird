ea4e46ee0cac810309bb2bd8a9aa21609c3d7d9c
Esteban Santana Santana <MentalPower@GMail.com>
2012-08-27 01:41:20 -0500

    Decided to tie almost all of the existing models (Town, Area, Status, Event & Action) to the Scan model to simplify ancestry tracking. Also reset the migration data for south (sorry).

--- app/prepa/migrations/0001_initial.py
+++ app/prepa/migrations/0001_initial.py
@@ -8,11 +8,20 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
+        # Adding model 'Scan'
+        db.create_table('prepa_scan', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('num_towns', self.gf('django.db.models.fields.IntegerField')(default=-1)),
+            ('num_breakdowns', self.gf('django.db.models.fields.IntegerField')(default=-1)),
+            ('record_time', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+        ))
+        db.send_create_signal('prepa', ['Scan'])
+
         # Adding model 'Town'
         db.create_table('prepa_town', (
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('name', self.gf('django.db.models.fields.CharField')(max_length=200)),
-            ('add_date', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('scan_added', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['prepa.Scan'], on_delete=models.PROTECT)),
         ))
         db.send_create_signal('prepa', ['Town'])
 
@@ -21,49 +30,53 @@ class Migration(SchemaMigration):
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('name', self.gf('django.db.models.fields.CharField')(max_length=200)),
             ('town', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['prepa.Town'], on_delete=models.PROTECT)),
-            ('add_date', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('scan_added', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['prepa.Scan'], on_delete=models.PROTECT)),
         ))
         db.send_create_signal('prepa', ['Area'])
-
-        # Adding model 'Event'
-        db.create_table('prepa_event', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('area', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['prepa.Area'], on_delete=models.PROTECT)),
-            ('add_date', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
-        ))
-        db.send_create_signal('prepa', ['Event'])
 
         # Adding model 'Status'
         db.create_table('prepa_status', (
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('name', self.gf('django.db.models.fields.CharField')(unique=True, max_length=200)),
             ('name_en', self.gf('django.db.models.fields.CharField')(max_length=200)),
-            ('add_date', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('scan_added', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['prepa.Scan'], on_delete=models.PROTECT)),
         ))
         db.send_create_signal('prepa', ['Status'])
+
+        # Adding model 'Event'
+        db.create_table('prepa_event', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('area', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['prepa.Area'], on_delete=models.PROTECT)),
+            ('scan_added', self.gf('django.db.models.fields.related.ForeignKey')(related_name='added', on_delete=models.PROTECT, to=orm['prepa.Scan'])),
+            ('scan_last_seen', self.gf('django.db.models.fields.related.ForeignKey')(related_name='last_seen', on_delete=models.PROTECT, to=orm['prepa.Scan'])),
+        ))
+        db.send_create_signal('prepa', ['Event'])
 
         # Adding model 'Action'
         db.create_table('prepa_action', (
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('event', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['prepa.Event'], on_delete=models.PROTECT)),
             ('status', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['prepa.Status'], on_delete=models.PROTECT)),
-            ('add_date', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('scan_added', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['prepa.Scan'], on_delete=models.PROTECT)),
         ))
         db.send_create_signal('prepa', ['Action'])
 
 
     def backwards(self, orm):
+        # Deleting model 'Scan'
+        db.delete_table('prepa_scan')
+
         # Deleting model 'Town'
         db.delete_table('prepa_town')
 
         # Deleting model 'Area'
         db.delete_table('prepa_area')
 
-        # Deleting model 'Event'
-        db.delete_table('prepa_event')
-
         # Deleting model 'Status'
         db.delete_table('prepa_status')
+
+        # Deleting model 'Event'
+        db.delete_table('prepa_event')
 
         # Deleting model 'Action'
         db.delete_table('prepa_action')
@@ -72,36 +85,44 @@ class Migration(SchemaMigration):
     models = {
         'prepa.action': {
             'Meta': {'object_name': 'Action'},
-            'add_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
             'event': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['prepa.Event']", 'on_delete': 'models.PROTECT'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'scan_added': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['prepa.Scan']", 'on_delete': 'models.PROTECT'}),
             'status': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['prepa.Status']", 'on_delete': 'models.PROTECT'})
         },
         'prepa.area': {
             'Meta': {'object_name': 'Area'},
-            'add_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'scan_added': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['prepa.Scan']", 'on_delete': 'models.PROTECT'}),
             'town': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['prepa.Town']", 'on_delete': 'models.PROTECT'})
         },
         'prepa.event': {
             'Meta': {'object_name': 'Event'},
-            'add_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
             'area': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['prepa.Area']", 'on_delete': 'models.PROTECT'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'scan_added': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'added'", 'on_delete': 'models.PROTECT', 'to': "orm['prepa.Scan']"}),
+            'scan_last_seen': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'last_seen'", 'on_delete': 'models.PROTECT', 'to': "orm['prepa.Scan']"})
+        },
+        'prepa.scan': {
+            'Meta': {'object_name': 'Scan'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'num_breakdowns': ('django.db.models.fields.IntegerField', [], {'default': '-1'}),
+            'num_towns': ('django.db.models.fields.IntegerField', [], {'default': '-1'}),
+            'record_time': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'})
         },
         'prepa.status': {
             'Meta': {'object_name': 'Status'},
-            'add_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '200'}),
-            'name_en': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+            'name_en': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'scan_added': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['prepa.Scan']", 'on_delete': 'models.PROTECT'})
         },
         'prepa.town': {
             'Meta': {'object_name': 'Town'},
-            'add_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'scan_added': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['prepa.Scan']", 'on_delete': 'models.PROTECT'})
         }
     }
 
--- app/prepa/models.py
+++ app/prepa/models.py
@@ -1,43 +1,42 @@
 from django.db import models
 
-# Create your models here.
+class Scan(models.Model):
+    num_towns = models.IntegerField(default=-1)
+    num_breakdowns = models.IntegerField(default=-1)
+    record_time = models.DateTimeField(auto_now_add=True)
+    def __unicode__(self):
+        return "%d towns and %d breakdowns scanned on %s" % (self.num_towns, self.num_breakdowns, self.record_time)
+
 class Town(models.Model):
     name = models.CharField(max_length=200)
-    add_date = models.DateTimeField(auto_now_add=True)
+    scan_added = models.ForeignKey(Scan, on_delete=models.PROTECT)
     def __unicode__(self):
         return self.name
 
 class Area(models.Model):
     name = models.CharField(max_length=200)
     town = models.ForeignKey(Town, on_delete=models.PROTECT)
-    add_date = models.DateTimeField(auto_now_add=True)
+    scan_added = models.ForeignKey(Scan, on_delete=models.PROTECT)
     def __unicode__(self):
         return "%s (%s)" % (self.town, self.name)
 
 class Status(models.Model):
     name = models.CharField(max_length=200, unique=True)
     name_en = models.CharField(max_length=200)
-    add_date = models.DateTimeField(auto_now_add=True)
+    scan_added = models.ForeignKey(Scan, on_delete=models.PROTECT)
     def __unicode__(self):
         return "%s (%s)" % (self.name, self.name_en)
 
 class Event(models.Model):
     area = models.ForeignKey(Area, on_delete=models.PROTECT)
-    add_date = models.DateTimeField(auto_now_add=True)
-    last_seen = models.DateTimeField(auto_now_add=True)
+    scan_added = models.ForeignKey(Scan, on_delete=models.PROTECT, related_name='added')
+    scan_last_seen = models.ForeignKey(Scan, on_delete=models.PROTECT, related_name='last_seen')
     def __unicode__(self):
-        return "%s:%s" % (self.area, unicode(self.add_date))
+        return "%s:%s:%s" % (self.area, self.scan_added, self.scan_last_seen)
 
 class Action(models.Model):
     event = models.ForeignKey(Event, on_delete=models.PROTECT)
     status = models.ForeignKey(Status, on_delete=models.PROTECT)
-    add_date = models.DateTimeField(auto_now_add=True)
+    scan_added = models.ForeignKey(Scan, on_delete=models.PROTECT)
     def __unicode__(self):
         return "%s-%s-%s" % (self.event, self.status, self.add_date)
-
-class Scan(models.Model):
-    add_date = models.DateTimeField(auto_now_add=True)
-    num_towns = models.IntegerField()
-    num_breakdowns = models.IntegerField()
-    def __unicode__(self):
-        return "%d towns and %d breakdowns scanned on %s" % (num_towns, num_breakdowns, add_date)
